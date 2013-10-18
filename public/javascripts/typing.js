fc1839be4e211e735690c67e06e74f8018c5e2b9
Zee Spencer <zee@zeespencer.com>
2013-10-18 18:31:26 -0400

    Refactor pass
    1. Encapsulated all the game state into a startTyping function so it wasn't
       hitting the global state
    2. Extracted a Timer object that I could test independently
    3. Moved the event listener from the DOM to the JavaScript
    4. Made it so the game didn't start until the window had finished loading.

--- public/javascripts/typing.js
+++ public/javascripts/typing.js
@@ -1,101 +1,103 @@
-var correctChars = '';
-var latestChar = 0;
-var totalCharsPressed = 0;
+function startTyping() {
 
-function gameLogic(event) {
+  var correctChars = '';
+  var latestChar = 0;
+  var totalCharsPressed = 0;
+  var timer = new Timer();
   var testString = document.getElementById("test").innerText;
-  concatenatingString(event, testString);
-  if (isDone(testString)) {
-    var timeInSeconds = (timer.endTime - timer.startTime) / 1000;
-    var wpm = wordsPerMinute(timeInSeconds, testString);
-    var accuracy = accuracyRating(totalCharsPressed,testString);
-    renderAccuracy("accuracy", accuracy);
-    renderSecondsElapsed("time-elapsed", timeInSeconds);
-    renderWPM("wpm", wpm);
+
+  function gameLogic(event) {
+    concatenatingString(keyPressed(event.keyCode), testString);
+    if (isDone(testString)) {
+      var timeInSeconds = (timer.endTime - timer.startTime) / 1000;
+      var wpm = wordsPerMinute(timeInSeconds, testString);
+      var accuracy = accuracyRating(totalCharsPressed,testString);
+      renderAccuracy("accuracy", accuracy);
+      renderSecondsElapsed("time-elapsed", timeInSeconds);
+      renderWPM("wpm", wpm);
+    }
   }
-}
 
-function isDone(testString) {
-  if (correctChars.length === testString.length) {
-    correctChars = '';
-    return true;
+  function isDone(testString) {
+    if (correctChars.length === testString.length) {
+      correctChars = '';
+      return true;
+    }
+    return false;
   }
-  return false;
-}
 
-function checkCorrect(char) {
-  var testString = document.getElementById("test").innerText;
-  ++totalCharsPressed;
-  if(char === testString[latestChar]) {
-    ++latestChar;
-    return true;
+  function checkCorrect(char, testString) {
+    ++totalCharsPressed;
+    if(char === testString[latestChar]) {
+      ++latestChar;
+      return true;
+    }
+    return false;
   }
-  return false;
-}
 
-function keyPressed(keycode) {
-  return String.fromCharCode(keycode);
-}
-
-function concatenatingString(event, string) {
-  var letter = keyPressed(event.keyCode);
-  if (checkCorrect(letter)) {
-    correctChars = correctChars.concat(letter);
-    setTimer(string);
-    renderString("text", correctChars);
+  function keyPressed(keycode) {
+    return String.fromCharCode(keycode);
   }
-}
 
-var timer = {
-  startTime: 0,
-  endTime: 0,
-  start: function() {
-    this.startTime += new Date().getTime();
-  },
-  end: function() {
-    this.endTime += new Date().getTime();
+  function concatenatingString(letter, string) {
+    if (checkCorrect(letter, string)) {
+      correctChars = correctChars.concat(letter);
+      setTimer(string);
+      renderString("text", correctChars);
+    }
   }
-};
 
-function setTimer(comparisonString) {
-  if (correctChars.length === 1) {
-    timer.start();
+  function setTimer(comparisonString) {
+    if (correctChars.length === 1) {
+      timer.start();
+    }
+    if (correctChars.length === comparisonString.length) {
+      timer.end();
+    }
   }
-  if (correctChars.length === comparisonString.length) {
-    timer.end();
+
+  function numberOfWords(string) {
+    var length = string.split(' ').length;
+    return length;
   }
+
+  function wordsPerMinute(time, string) {
+    var numberWordsInPrompt = numberOfWords(string);
+    var timeInMinutes = time/60;
+    var wpm = Math.floor(numberWordsInPrompt/timeInMinutes);
+    return wpm;
+  }
+
+  function accuracyRating(user_typed, actual_text) {
+    return Math.floor((actual_text.length)/user_typed * 100);
+  }
+
+  //VIEW
+
+  function bindEventListeners() {
+    document.getElementById('typing-zone').addEventListener('keypress', gameLogic);
+  }
+
+  function renderString(id, string) {
+    document.getElementById(id).innerText = string;
+  }
+
+  function renderSecondsElapsed(id, seconds) {
+    document.getElementById(id).innerText = "Seconds Elapsed: " + seconds;
+  }
+
+  function renderWPM(id, wpm) {
+    document.getElementById(id).innerText = "WPM: " + wpm;
+  }
+
+  function renderAccuracy(id, accuracy) {
+    document.getElementById(id).innerText = "Accuracy: " + accuracy + "%";
+  }
+
+  bindEventListeners();
 }
 
-function numberOfWords(string) {
-  var length = string.split(' ').length;
-  return length;
-}
 
-function wordsPerMinute(time, string) {
-  var numberWordsInPrompt = numberOfWords(string);
-  var timeInMinutes = time/60;
-  var wpm = Math.floor(numberWordsInPrompt/timeInMinutes);
-  return wpm;
-}
-
-function accuracyRating(user_typed, actual_text) {
-  return Math.floor((actual_text.length)/user_typed * 100);
-}
-
-//VIEW
-
-function renderString(id, string) {
-  document.getElementById(id).innerText = string;
-}
-
-function renderSecondsElapsed(id, seconds) {
-  document.getElementById(id).innerText = "Seconds Elapsed: " + seconds;
-}
-
-function renderWPM(id, wpm) {
-  document.getElementById(id).innerText = "WPM: " + wpm;
-}
-
-function renderAccuracy(id, accuracy) {
-  document.getElementById(id).innerText = "Accuracy: " + accuracy + "%";
+window.onload = function() {
+  startTyping();
 }
